aa4a62b9fbc09b40c6b510dddb3d307a61ac897f
Cory Welch <cory.welch77@gmail.com>
2018-05-30 00:10:23 -0400

    Fixing Updatednsmasq line in update scripts

--- DNSDHCPServer/Master/updatescripts.sh
+++ DNSDHCPServer/Master/updatescripts.sh
@@ -8,4 +8,4 @@
 
 curl https://raw.githubusercontent.com/corywelch/WelchServer/master/DNSDHCPServer/Master/makehosts.sh > /home/pi/config/makehosts.sh
 curl https://raw.githubusercontent.com/corywelch/WelchServer/master/DNSDHCPServer/Master/setupdnsmasq.sh > /home/pi/config/setupdnsmasq.sh
-curl https://raw.githubusercontent.com/corywelch/WelchServer/master/DNSDHCPServer/Master/updatedsnmasq.sh > /home/pi/config/updatednsmasq.sh
+curl https://raw.githubusercontent.com/corywelch/WelchServer/master/DNSDHCPServer/Master/updatednsmasq.sh > /home/pi/config/updatednsmasq.sh
